e0bf923feb7a783b237c62a047a226884a20adab
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-03-28 19:38:24 +0000

    a

--- games/tic-tac-toe/implementing -game-state/src/index.js
+++ games/tic-tac-toe/implementing -game-state/src/index.js
@@ -5,6 +5,6 @@ const ctx = canvas.getContext('2d');
 const board = createBoard(ctx); // Pass the context to createBoard
 
 drawBoard(board);
-drawO(ctx,1,1)
-drawX(ctx,0,0)
+drawO(ctx,1,1);
+drawX(ctx,0,0);
 
--- games/tic-tac-toe/implementing -game-state/src/utils/index.js
+++ games/tic-tac-toe/implementing -game-state/src/utils/index.js
@@ -1,2 +1,1 @@
-export {createMachine} from './machine.js';
 export {createBoard, drawBoard, drawO, drawX} from './board.js';
